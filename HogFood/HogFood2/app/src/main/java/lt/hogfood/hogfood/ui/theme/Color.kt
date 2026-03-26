ddc410ed0fa6af2f5b7980bc455736f856d3a59a
Camille <kamile10za@gmail.com>
2026-03-27 00:11:51 +0200

    feat: setup theme colors and typography

--- HogFood/HogFood2/app/src/main/java/lt/hogfood/hogfood/ui/theme/Color.kt
+++ HogFood/HogFood2/app/src/main/java/lt/hogfood/hogfood/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package lt.hogfood.hogfood.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryBlue = Color(0xFF185FA5)
+val HeartRed = Color(0xFFE24B4A)
+val StarAmber = Color(0xFFEF9F27)
+val TextPrimary = Color(0xFF1A1A1A)
+val TextSecondary = Color(0xFF888888)
+val CardBackground = Color(0xFFF5F5F3)
+val AiBoxBackground = Color(0xFFE6F1FB)
+val SurfaceWhite = Color(0xFFFFFFFF)
